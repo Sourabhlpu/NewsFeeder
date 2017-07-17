7baaeb599eefd11536fe22373831b2aff39feb0e
Sourabh Pal <nick.tdr19@gmail.com>
2017-07-18 00:03:27 +0530

    Add comments and documentation for MainActivity and ArticleLoader Classes

--- app/src/main/java/com/example/personal/newsfeeder/ArticleLoader.java
+++ app/src/main/java/com/example/personal/newsfeeder/ArticleLoader.java
@@ -3,17 +3,24 @@ package com.example.personal.newsfeeder;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by personal on 3/16/2017.
+ * this is the class that does all the background work loading the data for the loader
+ * it extends from the AsyncTaskLoader and overrides the important methods
  */
 
 public class ArticleLoader extends AsyncTaskLoader<List<TheArticle>> {
 
+    // mURL stores the url for fetching the data.
     private String mURL;
 
+    /*
+     * this constructor initializes the mURL
+     * @param context is the context that's required by the super class
+     * @param url is the url that is used to fetch the data
+     */
     public ArticleLoader(Context context,String url)
     {
         super(context);
@@ -23,9 +30,16 @@ public class ArticleLoader extends AsyncTaskLoader<List<TheArticle>> {
     @Override
     protected void onStartLoading() {
 
+        //this method ignores the previously loaded data and load a new one.
         forceLoad();
     }
 
+    /*
+     * this is the only method that runs in the background thread
+     * it validates the url and returns if its null.
+     * the fetchArticles of the queryUtils uses this url to fetch the JSON Data.
+     * this method finally returns the articles that are returned by the fetchArticles method
+     */
     @Override
     public List<TheArticle> loadInBackground() {
         if(mURL == null)
--- app/src/main/java/com/example/personal/newsfeeder/MainActivity.java
+++ app/src/main/java/com/example/personal/newsfeeder/MainActivity.java
@@ -22,81 +22,134 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * The main activity implements the LoaderManager to handle all the loading of data off the main thread
+ * we also implement ListItemOnClickHandler to handle the clicks on the article
+*/
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<TheArticle>>,
-RVAdapter.ListItemOnClickHandler{
+        RVAdapter.ListItemOnClickHandler {
+
+    //this is the string that we will use to fetch the json data.
+
     private static final String NEWS_REQUEST_URL = "http://content.guardianapis.com/" +
             "search";
+    //we give the loader an id. This is just a random unique value.
     private static final int EARTHQUAKE_LOADER_ID = 1;
+
+    //The api key that we need to pass in with our request to the server of the Gaurdian
+    private static final String API_KEY = "ce10d58e-3c68-451c-beb3-7b6a79f5b75f";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static boolean usingSearch = false;
     private RecyclerView mRecyclerView;
     private RVAdapter mAdapter;
     private LinearLayoutManager mLayoutManager;
     private TextView mEmptyTextView;
-
-    private static final String API_KEY = "ce10d58e-3c68-451c-beb3-7b6a79f5b75f";
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static boolean usingSearch = false;
-
     private EndlessRecyclerViewScrollListener scrollListener;
     private Uri.Builder uriBuilder;
-    private String page = "1";
+    private  String page = "1";
 
-    public String createAPIQueryString(String currentPage)
-    {
-        Log.v(LOG_TAG,"The value of the page index " + currentPage);
+    /*
+      * this method will build our string to query for the right data
+      * it will return the query String
+      * @param currentPage is passed in which is the current article page we have
+     */
+
+    public String createAPIQueryString(String currentPage) {
+        Log.v(LOG_TAG, "The value of the page index " + currentPage);
+
+        //we fetch the settings the user has set to fetch the data in the settings activity.
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //we want to get the value of the user preference of the sort order of the articles
         String orderBy = sharedPrefs.getString(getString(R.string.settings_order_by_key),
                 getString(R.string.settings_orderby_default));
+        // this setting of user will set that how many pages we want to load per query.
         String pageSize = sharedPrefs.getString(getString(R.string.settings_page_size_key),
                 getString(R.string.settings_page_size_default));
+
+        /*
+         * now we are done with fetching the user preferences
+         * It is time  to create he uri. We will add the above user settings to the request url
+         */
+
+        //this takes the query string saved as string constant and converts it into a URI.
+        // Now we can use this uri to add parameters to it.
 
         Uri baseUri = Uri.parse(NEWS_REQUEST_URL);
         uriBuilder = baseUri.buildUpon();
 
-        if(orderBy.equals("oldest"))
-        {
-            uriBuilder.appendQueryParameter("from-date","2017-01-01");
-            uriBuilder.appendQueryParameter("use-date","published");
+        if (orderBy.equals("oldest")) {
+            uriBuilder.appendQueryParameter("from-date", "2017-01-01");
+            uriBuilder.appendQueryParameter("use-date", "published");
 
         }
         // if(orderBy.equals("relevance") )
 
-        uriBuilder.appendQueryParameter("order-by",orderBy);
-        uriBuilder.appendQueryParameter("show-fields","byline,thumbnail,trailText,body");
-        uriBuilder.appendQueryParameter("page",currentPage);
-        uriBuilder.appendQueryParameter("page-size",pageSize);
-        uriBuilder.appendQueryParameter("api-key",API_KEY);
 
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("show-fields", "byline,thumbnail,trailText,body");
+        uriBuilder.appendQueryParameter("page", currentPage);
+        uriBuilder.appendQueryParameter("page-size", pageSize);
+        uriBuilder.appendQueryParameter("api-key", API_KEY);
+
+        // this is the sample url thats created with a few appended parameters
+        // http://content.guardianapis.com/search?order-by=relevance&use-date=published&api-key=ce10d58e-3c68-451c-beb3-7b6a79f5b75f
+
+        // convert the uri to string and then return it.
         return uriBuilder.toString();
 
     }
+
+    //Overriding the three loader methods
+
+    /*
+     * this method is called when the loader is created.
+     * it uses the createAPIQueryString to create a query for the api
+     * the it passes that string with the context to the ArticleLoader object
+     */
 
     @Override
     public Loader<List<TheArticle>> onCreateLoader(int id, Bundle args) {
 
 
-       String queryString = createAPIQueryString(page);
+        String queryString = createAPIQueryString(page);
 
-        Log.v("The url",LOG_TAG + uriBuilder.toString());
+        Log.v("The url", LOG_TAG + uriBuilder.toString());
 
         return new ArticleLoader(this, uriBuilder.toString());
     }
 
+    /*
+     * this method is called when the loading of data is done
+     * this method does all the work of updating the UI
+     * updating of UI happens via updating the adapter.
+     */
+
     @Override
     public void onLoadFinished(Loader<List<TheArticle>> loader, List<TheArticle> articles) {
 
+        //make the progress bar invisible as the data load has finished
         findViewById(R.id.progress_bar).setVisibility(RecyclerView.GONE);
+
+        //check if we were successfully able to load the data by checking if the articles list is empty
+        // if its not empty then update the adapter
         if (articles != null && !articles.isEmpty()) {
-            //articles.clear();
 
+            //this method updates the adapter.
             mAdapter.updateDataset(articles, mRecyclerView);
-            Log.v(LOG_TAG,"The articles object is " + articles);
+            Log.v(LOG_TAG, "The articles object is " + articles);
 
-        } else {
+        }
+        // if the articles list is not loaded then show the error message.
+        else {
             mEmptyTextView.setText("No atricles found");
             findViewById(R.id.progress_bar).setVisibility(RecyclerView.GONE);
         }
 
     }
+
+    // for now we don't do anything with this method
 
     @Override
     public void onLoaderReset(Loader<List<TheArticle>> loader) {
@@ -110,24 +163,28 @@ RVAdapter.ListItemOnClickHandler{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_view);
 
+        //mEmptyTextView is used to display an error message when we cannot load the data
         mEmptyTextView = (TextView) findViewById(R.id.empty_text_view);
+
+        //this is the recycler view that displays the data in the list
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
+
+        //check if we are connected to the internet
         ConnectivityManager cm = (ConnectivityManager) this.getSystemService(this.CONNECTIVITY_SERVICE);
         NetworkInfo info = cm.getActiveNetworkInfo();
 
-        if(info != null && info.isConnected())
-        {
+        //if we are connected then start the loader
+        if (info != null && info.isConnected()) {
             LoaderManager loaderManager = getLoaderManager();
             loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
         }
-        else
-        {
+        // otherwise hide the progress bar and show the appropriate error message
+        else {
             findViewById(R.id.progress_bar).setVisibility(RecyclerView.GONE);
             mEmptyTextView.setText("No internet connection");
         }
-
 
 
         mRecyclerView.setHasFixedSize(true);
@@ -139,12 +196,34 @@ RVAdapter.ListItemOnClickHandler{
 
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-
-
+        /*
+         * initializing the adapter
+         * the first param takes the context of this activity to pass the context
+         * the second param is passed the an article list object to initialize the list in the adapter
+         * the third param is passed this coz this activity implements the onClick method of the
+         * listItemOnclickHandler abstract class.
+         *
+         */
         mAdapter = new RVAdapter(this, new ArrayList<TheArticle>(), this);
         mRecyclerView.setAdapter(mAdapter);
 
+        /*
+         * EndlessRecyclerViewScrollListener class is an abstract class that extends RecyclerView's
+         * OnScrollListener class. This is used for infinite loading of data
+         * Here we initialize the class. We pass in the layoutManager that our recycler uses
+         *
+         */
+
         scrollListener = new EndlessRecyclerViewScrollListener(mLayoutManager) {
+
+            /*
+              * onLoadMore method is implemented here which uses currentPage to load data for the next page
+              * @param currentPage is very important here. The api uses current page parameter to load pages
+                from the database.
+              * When the threshold is reached in the onScrolled method of the abstract class we increment
+                the currentPage number and pass it to the onLoadMore method
+             */
+
             @Override
             public void onLoadMore(int currentPage, int totalItemCount, RecyclerView recyclerView) {
 
@@ -153,6 +232,8 @@ RVAdapter.ListItemOnClickHandler{
             }
         };
 
+        //adding the onScrollListener for the recycler view
+
         mRecyclerView.addOnScrollListener(scrollListener);
 
         scrollListener.resetState();
@@ -160,26 +241,38 @@ RVAdapter.ListItemOnClickHandler{
 
     }
 
+    /*
+      * here in this method we finally take the currentPage value and the update the page string
+      * so before the loader is destroyed and restarted the page variable is updated and the next page
+      * is loaded
+     */
     public void loadNextDataFromApi(int currentPage) {
         page = currentPage + "";
         //Log.v(LOG_TAG,"PAGE VALUE IN THE load.. function " + page);
         getLoaderManager().destroyLoader(EARTHQUAKE_LOADER_ID);
-        getLoaderManager().restartLoader(EARTHQUAKE_LOADER_ID,null,this);
+        getLoaderManager().restartLoader(EARTHQUAKE_LOADER_ID, null, this);
     }
+
+    /*
+     * this method creates the menu for our main activity
+     */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    /*
+     * we use this method to open the settings activity using intents
+     */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        
-        if(id == R.id.settings)
-        {
-           Intent intent = new Intent(this,SettingsActivity.class);
+
+        if (id == R.id.settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
         }
@@ -197,7 +290,7 @@ RVAdapter.ListItemOnClickHandler{
     public void onClick(TheArticle article) {
         Context context = this;
         Class destinationClass = DetailActivity.class;
-        Intent intentToStartDetailActivity = new Intent(context,destinationClass);
+        Intent intentToStartDetailActivity = new Intent(context, destinationClass);
 
         intentToStartDetailActivity.putExtra(getString(R.string.putExtra_avatar_name),
                 article.getmAvatarName());
